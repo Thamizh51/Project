f6345169588c6636eb09ab2a060b34aab00053a5
Thamizhselvan <tamildharani51@gmail.com>
2026-02-25 16:37:49 +0530

    Initial commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import "./Navbar.css";
+import "./navbar.css";
 
 function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
